bf0d55d79903528377d051fa8b90e9070666055e
shymaSane <shymaSane1@gmail.com>
2018-11-18 14:36:10 +0200

    display lyrics for certain id

--- src/component/lyrics/Lyrics.js
+++ src/component/lyrics/Lyrics.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios'
 
 export default class Lyrics extends Component {
-  render() {
+
+    state={
+        song_name: "Lyrics:",
+        lyrics: ''
+    }
+
+    async componentDidMount(){
+        const {id} = this.props.match.params
+        const res = await axios.get     (`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${process.env.REACT_APP_MUISXMATCH_KEY}`);
+        const data = await res.data;
+        const body = await data.message.body.lyrics.lyrics_body;
+        this.setState({
+                lyrics : body
+        })
+    }
+
+  render(props) {
     return (
         <div>
-            <a href="/" className="btn btn-outline-danger">Go Back</a>
+            <Link to="/" className="btn btn-outline-danger">Go Back</Link>
             <div className="card">
-                <div className="card-header">song name</div>
+                <div className="card-header">{this.state.song_name}</div>
                 <div className="card-body">
                     <p className="card-text">
-                        lyrics
+                        {this.state.lyrics}
                     </p>
                 </div>
             </div>
@@ -18,3 +36,4 @@ export default class Lyrics extends Component {
     )
   }
 }
+
